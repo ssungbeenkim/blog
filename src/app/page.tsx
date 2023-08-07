a9bc77ba5c3ec09dfc879dd5044d460c07114eec
wandakim <nwd1583@naver.com>
2023-08-08 06:56:33 +0900

    chore: update typescript, types/react for Async Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedPosts from '@/conponents/FeaturedPosts';
 import Hero from '@/conponents/Hero';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      @ts-expect-error Async Server Component
+      <FeaturedPosts />
     </>
   );
 }
