4691599890da8f25a60b17fbf72ee55fcc520cf5
wandakim <nwd1583@naver.com>
2023-08-10 15:41:56 +0900

    refactor: post page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import MarkdownViewer from '@/conponents/MarkdownViewer';
+import PostContent from '@/conponents/PostContent';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
-import { BsFillCalendarDateFill } from 'react-icons/bs';
 
 type Props = {
   params: {
@@ -9,7 +8,8 @@ type Props = {
   };
 };
 export default async function Page({ params: { slug } }: Props) {
-  const { title, description, date, path, content } = await getPostData(slug);
+  const post = await getPostData(slug);
+  const { title, path } = post;
   return (
     <article className='m-4 overflow-hidden rounded-2xl bg-gray-100 shadow-lg'>
       <Image
@@ -19,16 +19,7 @@ export default async function Page({ params: { slug } }: Props) {
         width={760}
         height={420}
       />
-      <section className='flex flex-col'>
-        <div className='flex items-center self-end text-gray-500'>
-          <BsFillCalendarDateFill />
-          <p className='ml-2 font-semibold'>{date.toString()}</p>
-        </div>
-        <h1 className='text-4xl font-bold'>{title}</h1>
-        <p className='text-xl font-bold'>{description}</p>
-        <div className='mt-4 mb-8 w-44 border-2 border-sky-600'></div>
-        <MarkdownViewer content={content} />
-      </section>
+      <PostContent post={post} />
     </article>
   );
 }
---
+++ src/conponents/PostContent.tsx
@@ -0,0 +1,19 @@
+import { PostData } from '@/service/posts';
+import MarkdownViewer from './MarkdownViewer';
+import { BsFillCalendarDateFill } from 'react-icons/bs';
+
+export default function PostContent({ post }: { post: PostData }) {
+  const { title, description, date, content } = post;
+  return (
+    <section className='flex flex-col'>
+      <div className='flex items-center self-end text-gray-500'>
+        <BsFillCalendarDateFill />
+        <p className='ml-2 font-semibold'>{date.toString()}</p>
+      </div>
+      <h1 className='text-4xl font-bold'>{title}</h1>
+      <p className='text-xl font-bold'>{description}</p>
+      <div className='mt-4 mb-8 w-44 border-2 border-sky-600'></div>
+      <MarkdownViewer content={content} />
+    </section>
+  );
+}
