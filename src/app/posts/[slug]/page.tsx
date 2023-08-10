10e723b47de66af6b3e77e9fb173cd3e8ecd013a
wandakim <nwd1583@naver.com>
2023-08-10 14:29:13 +0900

    feat: markdown viewer - description

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import MarkdownViewer from '@/conponents/MarkdownViewer';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
-
+import { BsFillCalendarDateFill } from 'react-icons/bs';
 type Props = {
   params: {
     slug: string;
@@ -10,15 +10,22 @@ type Props = {
 export default async function Page({ params: { slug } }: Props) {
   const { title, description, date, path, content } = await getPostData(slug);
   return (
-    <article>
+    <article className='m-4 overflow-hidden rounded-2xl bg-gray-100 shadow-lg'>
       <Image
-        src={`/image/posts/${path}.png`}
+        className='h-7/1 w-full object-cover'
+        src={`/images/posts/${path}.png`}
         alt={title}
         width={760}
         height={420}
       />
-      <section>
-        <h1>{title}</h1>
+      <section className='flex flex-col'>
+        <div className='flex items-center self-end text-gray-500'>
+          <BsFillCalendarDateFill />
+          <p className='ml-2 font-semibold'>{date.toString()}</p>
+        </div>
+        <h1 className='text-4xl font-bold'>{title}</h1>
+        <p className='text-xl font-bold'>{description}</p>
+        <div className='mt-4 mb-8 w-44 border-2 border-sky-600'></div>
         <MarkdownViewer content={content} />
       </section>
     </article>
