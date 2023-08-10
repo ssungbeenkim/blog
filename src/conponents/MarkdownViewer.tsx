9bc92f64605ceaba2ba646540fd302b396e4461a
wandakim <nwd1583@naver.com>
2023-08-10 13:11:03 +0900

    chore: Add tailwindcss/typography

--- src/conponents/MarkdownViewer.tsx
+++ src/conponents/MarkdownViewer.tsx
@@ -2,5 +2,9 @@ import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import remarkGfm from 'remark-gfm';
 
 export default function MarkdownViewer({ content }: { content: string }) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown className='prose lg:prose-xl' remarkPlugins={[remarkGfm]}>
+      {content}
+    </ReactMarkdown>
+  );
 }
