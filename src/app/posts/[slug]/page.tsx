a89c768cd10ee79f14f656b346d1bb7cdeb196ed
wandakim <nwd1583@naver.com>
2023-08-10 13:16:27 +0900

    chore: Add react-icons

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import MarkdownViewer from '@/conponents/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
 
 type Props = {
   params: {
@@ -7,11 +8,19 @@ type Props = {
   };
 };
 export default async function Page({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
   return (
-    <>
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </>
+    <article>
+      <Image
+        src={`/image/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section>
+        <h1>{title}</h1>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
